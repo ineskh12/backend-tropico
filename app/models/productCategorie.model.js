4a3f636c8ca48e1f2834589f4d75179a210ed39e
ines khelifi <ines.khelifi.1@esprit.tn>
2021-03-24 16:15:07 +0100

    test3

--- app/models/productCategorie.model.js
+++ app/models/productCategorie.model.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const ProductCategorieSchema = mongoose.Schema({
+
     categorieAr:{type:String, enum:['خضر','غلال','أسماك','لحوم']},
-    categorieFr:{type:String, enum:['viandes','poissons','fruits','legumes']},
-    id: {type:Number, enum:[1,2,3,4]}
+categorieFr:{type:String, enum: ['viandes', 'poissons','fruits','legumes']},
+categorieid: {type:Number, enum:[1,2,3,4],default: '4'}
 
 }, {
     timestamps: true
